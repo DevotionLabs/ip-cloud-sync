f6dfab55cb909cf6453baef58b82e87d6631d697
diego <diego.undevfined@gmail.com>
2025-01-05 19:38:03 +0100

    Application setup

---
+++ app/src/main/kotlin/labs/devotion/ipcloudsync/config/Config.kt
@@ -0,0 +1,13 @@
+object ConfigManager {
+
+    private val environment: Map<String, String> = System.getenv()
+
+    enum class ConfigKey(val key: String) {
+        IP_FETCH_SERVER_URL("IPCLOUDSYNC_IP_FETCH_SERVER");
+    }
+
+    fun getEnv(configKey: ConfigKey, default: String? = null): String {
+        return environment[configKey.key] ?: default
+        ?: throw IllegalStateException("Environment variable '\${configKey.key}' is not set and no default value was provided.")
+    }
+}

8bfb078d80cdd18950932dcc2caf06d48f9a528a
diego <diego.undevfined@gmail.com>
2025-01-05 19:55:27 +0100

    Add logger

---
+++ app/src/main/kotlin/labs/devotion/ipcloudsync/logger/Color.kt
@@ -0,0 +1,12 @@
+package labs.devotion.ipcloudsync.logger
+
+enum class Color(val value: String) {
+    BLUE("\u001B[34m"),
+    YELLOW("\u001B[33m"),
+    RED("\u001B[31m"),
+    RESET("\u001B[0m");
+
+    override fun toString(): String {
+        return value
+    }
+}
---
+++ app/src/main/kotlin/labs/devotion/ipcloudsync/logger/Logger.kt
@@ -0,0 +1,29 @@
+package labs.devotion.ipcloudsync.logger
+
+import java.time.LocalDateTime
+
+object Logger {
+
+    enum class LogLevel(val color: Color) {
+        INFO(Color.BLUE),
+        WARNING(Color.YELLOW),
+        ERROR(Color.RED);
+    }
+
+    fun info(message: String) {
+        log(message, LogLevel.INFO)
+    }
+
+    fun warning(message: String) {
+        log(message, LogLevel.WARNING)
+    }
+
+    fun error(message: String) {
+        log(message, LogLevel.ERROR)
+    }
+
+    private fun log(message: String, level: LogLevel = LogLevel.INFO) {
+        val timestamp = LocalDateTime.now()
+        println("\${level.color}[\$timestamp] [\${level.name}] \$message\$ ${Color.RESET}")
+    }
+}

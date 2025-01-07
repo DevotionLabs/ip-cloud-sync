9721ad87f0978e59ecabdd3cca23b8b2030c0578
diego <diego.undevfined@gmail.com>
2025-01-08 00:32:46 +0100

    Define IP fetcher

---
+++ app/src/main/kotlin/labs/devotion/ipcloudsync/ipfetcher/IpFetcher.kt
@@ -0,0 +1,13 @@
+package labs.devotion.ipcloudsync.ipfetcher
+
+import labs.devotion.ipcloudsync.httpclient.HttpClient
+import labs.devotion.ipcloudsync.iptools.IpTools
+
+class IpFetcher(private val httpClient: HttpClient) {
+
+    fun fetchPublicIp(): String {
+        val ip = httpClient.get("/")
+        return IpTools.validateIp(ip)
+    }
+
+}
---
+++ app/src/test/kotlin/labs/devotion/ipcloudsync/ipfetcher/IpFetcherTest.kt
@@ -0,0 +1,51 @@
+package labs.devotion.ipcloudsync.ipfetcher
+
+import labs.devotion.ipcloudsync.httpclient.HttpClient
+import labs.devotion.ipcloudsync.httpclient.Protocol
+import labs.devotion.ipcloudsync.iptools.IpTools
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+
+class IpFetcherTest {
+
+    private lateinit var mockWebServer: MockWebServer
+    private lateinit var httpClient: HttpClient
+
+    @Before
+    fun setUp() {
+        mockWebServer = MockWebServer()
+        mockWebServer.start()
+        httpClient = HttpClient(Protocol.HTTP, mockWebServer.hostName + ":" + mockWebServer.port)
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun `fetch valid IP`() {
+        val validIp = "192.168.1.1"
+        val mockResponse = MockResponse().setResponseCode(200).setBody(validIp)
+        mockWebServer.enqueue(mockResponse)
+
+        val ip = IpFetcher(httpClient).fetchPublicIp()
+        assertTrue(IpTools.isValidIp(ip), "Expected a valid IP address, but got: $ip")
+    }
+
+    @Test
+    fun `fetch invalid IP`() {
+        val invalidIp = "invalid_ip"
+        mockWebServer.enqueue(MockResponse().setResponseCode(200).setBody(invalidIp))
+
+        val ipFetcher = IpFetcher(httpClient)
+        assertFailsWith<IllegalArgumentException> {
+            ipFetcher.fetchPublicIp()
+        }
+    }
+}

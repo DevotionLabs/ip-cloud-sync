b2391526a05b12cd1790c8cb3710246782219764
diego <diego.undevfined@gmail.com>
2025-01-08 14:41:07 +0100

    Extract config keys from Config class

--- app/src/main/kotlin/labs/devotion/ipcloudsync/config/Config.kt
+++ app/src/main/kotlin/labs/devotion/ipcloudsync/config/Config.kt
@@ -1,13 +1,11 @@
+package labs.devotion.ipcloudsync.config
+
 object Config {
 
     private val environment: Map<String, String> = System.getenv()
 
-    enum class ConfigKey(val key: String) {
-        IP_FETCH_SERVER_URL("IPCLOUDSYNC_IP_FETCH_SERVER_URL");
-    }
-
-    fun getEnv(configKey: ConfigKey, default: String? = null): String {
-        return environment[configKey.key] ?: default
+    fun getEnv(configKey: ConfigKeys): String {
+        return environment[configKey.key] ?: configKey.defaultValue
             ?: throw IllegalStateException("Environment variable '\${configKey.key}' is not set and no default value was provided.")
     }
 }
---
+++ app/src/main/kotlin/labs/devotion/ipcloudsync/config/ConfigKeys.kt
@@ -0,0 +1,9 @@
+package labs.devotion.ipcloudsync.config
+
+enum class ConfigKeys(val key: String, val defaultValue: String? = null) {
+    CLOUDFLARE_API_URL("IPCLOUDSYNC_CLOUDFLARE_API_URL", "api.cloudflare.com"),
+    CLOUDFLARE_API_TOKEN("IPCLOUDSYNC_CLOUDFLARE_API_TOKEN"),
+    CRON_FREQUENCY_MINS("IPCLOUDSYNC_CRON_FREQUENCY_MINS", "60"),
+    DOMAIN("IPCLOUDSYNC_DOMAIN"),
+    IP_FETCH_SERVER_URL("IPCLOUDSYNC_IP_FETCH_SERVER_URL", "api.ipify.org");
+}

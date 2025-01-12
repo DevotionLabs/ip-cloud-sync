85e1ee18a800395f25f48a2a7b7c7fa2fc1d4e30
diego <diego.undevfined@gmail.com>
2025-01-12 20:25:44 +0100

    Refresh environment in each access

--- app/src/main/kotlin/labs/devotion/ipcloudsync/config/Config.kt
+++ app/src/main/kotlin/labs/devotion/ipcloudsync/config/Config.kt
@@ -2,9 +2,9 @@ package labs.devotion.ipcloudsync.config
 
 object Config {
 
-    private val environment: Map<String, String> = System.getenv()
-
     fun getEnv(configKey: ConfigKeys): String {
+        val environment = System.getenv()
+
         return environment[configKey.key] ?: configKey.defaultValue
             ?: throw IllegalStateException("Environment variable '\${configKey.key}' is not set and no default value was provided.")
     }

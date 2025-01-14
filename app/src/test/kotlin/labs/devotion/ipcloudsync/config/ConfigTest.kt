feb5bad79d27b72c408180060f710306cbbdb1cd
diego <diego.undevfined@gmail.com>
2025-01-14 12:03:17 +0100

    Upgrade gradle to 8.12 to build JARs

--- app/src/test/kotlin/labs/devotion/ipcloudsync/config/ConfigTest.kt
+++ app/src/test/kotlin/labs/devotion/ipcloudsync/config/ConfigTest.kt
@@ -1,41 +1,14 @@
 package labs.devotion.ipcloudsync.config
 
-import com.github.stefanbirkner.systemlambda.SystemLambda.withEnvironmentVariable
-import labs.devotion.ipcloudsync.logger.LogLevel
-import labs.devotion.ipcloudsync.logger.Logger
 import kotlin.test.Test
-import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 class ConfigTest {
 
     @Test
-    fun `should set log level based on Config`() {
-        withEnvironmentVariable(ConfigKeys.LOG_LEVEL.key, "DEBUG").execute {
-            val logLevel = Config.getEnv(ConfigKeys.LOG_LEVEL)
-
-            Logger.setLevel(logLevel)
-
-            assertEquals(LogLevel.DEBUG, Logger.getLevel())
-        }
-
-        Logger.setLevel("INFO") // Reset
-    }
-
-    @Test
     fun `should throw an error on non-existing env`() {
         assertFailsWith<IllegalStateException> {
             Config.getEnv(ConfigKeys.CLOUDFLARE_API_TOKEN)
-        }
-    }
-
-    @Test
-    fun `should use a default value when it is set`() {
-        val logLevelConfig = ConfigKeys.LOG_LEVEL
-        withEnvironmentVariable(logLevelConfig.key, null).execute {
-            val logLevel = Config.getEnv(logLevelConfig)
-
-            assertEquals(logLevelConfig.defaultValue, logLevel)
         }
     }
 }

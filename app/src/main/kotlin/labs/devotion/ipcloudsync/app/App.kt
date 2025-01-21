3d2ea8f099480456c37b380f8c04586b5836ea70
diego <diego.undevfined@gmail.com>
2025-01-21 15:05:40 +0100

    Extract setup functions from App.start()

--- app/src/main/kotlin/labs/devotion/ipcloudsync/app/App.kt
+++ app/src/main/kotlin/labs/devotion/ipcloudsync/app/App.kt
@@ -11,22 +11,39 @@ object App {
     private lateinit var ipSyncer: IpSyncer
 
     fun start() {
-        val logLevel = Config.getEnv(ConfigKeys.LOG_LEVEL)
-        Logger.setLevel(logLevel)
+        setupLogger()
         Logger.info("IP CloudSync application started")
 
-        val frequencyMins = Config.getEnv(ConfigKeys.CRON_FREQUENCY_MINS)
-        ipSyncer = IpSyncer(frequencyMins.toLong())
-
-        handleTerminationSignals()
-
         try {
+            initIpSyncer()
+
+            handleTerminationSignals()
+
             ipSyncer.start()
             Logger.info("Application is running. Press Ctrl+C to exit.")
         } catch (e: Exception) {
             Logger.error("Application could not be started: ${e.message}")
             exitProcess(1)
         }
+    }
+
+    private fun setupLogger() {
+        val logLevel = Config.getEnv(ConfigKeys.LOG_LEVEL)
+        Logger.setLevel(logLevel)
+    }
+
+    private fun initIpSyncer() {
+        val frequencyMinsEnvValue = Config.getEnv(ConfigKeys.CRON_FREQUENCY_MINS)
+
+        // TODO: Extract this to another function
+        val frequencyMins = try {
+            frequencyMinsEnvValue.toLong()
+        } catch (e: NumberFormatException) {
+            ConfigKeys.CRON_FREQUENCY_MINS.defaultValue?.toLongOrNull()
+                ?: throw NumberFormatException("Default frequency value must be a number")
+        }
+
+        ipSyncer = IpSyncer(frequencyMins)
     }
 
     private fun handleTerminationSignals() {
